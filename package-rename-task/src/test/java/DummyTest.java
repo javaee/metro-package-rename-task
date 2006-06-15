f2b40cd560b2effdaea68b3deb409bc114fbf638
Kohsuke Kawaguchi <kohsuke@java.net>
2006-06-15 19:36:15 +0000

    added junit dependency for test.
    svn path=/trunk/; revision=7

---
+++ package-rename-task/src/test/java/DummyTest.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+
+/**
+ * If there's no test, Maven isn't happy.
+ * @author Kohsuke Kawaguchi
+ */
+public class DummyTest extends TestCase {
+    public void test1() {}
+}

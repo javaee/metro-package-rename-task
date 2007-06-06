3abe93a5155de2c00fd000aceebdf8a6b2bdfdc9
Kohsuke Kawaguchi <kohsuke@java.net>
2007-06-06 22:59:18 +0000

    added a test case for the exclusion.
    svn path=/trunk/; revision=21

---
+++ package-rename-task/src/test/java/org/acme/foo/japan/islands/Okinawa.java
@@ -0,0 +1,13 @@
+package org.acme.foo.japan.islands;
+
+import org.acme.foo.japan.Osaka;
+import org.acme.foo.japan.Tokyo;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class Okinawa {
+    Osaka osaka;
+    Tokyo tokyo;
+    Okinawa okinawa;
+}
